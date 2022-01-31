317fef7b5a2dafc8f3992d771f4282366627f35f
Monteguen <montegue.n@gmail.com>
2022-01-31 11:55:34 +0300

    timer

--- app/pages/Quotes/Quotes.tsx
+++ app/pages/Quotes/Quotes.tsx
@@ -4,29 +4,36 @@ import { View, Text,} from 'react-native';
 import { getQuotes, Quote } from '../../requests';
 const Quotes = () => {
   const cleanupFunction = React.useRef(false)
+  const [seconds, setSeconds] = React.useState(0);
   const [collectionQuotes, setCollectionQuotes] = React.useState<Quote[]>([])
   const updateQuotes = () => {
     getQuotes().then((res) => {
      if (res && !cleanupFunction.current) {
       setCollectionQuotes(res)
+      const d = new Date()
+      setSeconds(d.getTime())
      }
      })
   }
-    React.useEffect(()=>{
-      updateQuotes()
-      return () => {
-        cleanupFunction.current = true
-    }
-    },[])
     useFocusEffect(
       React.useCallback(() => {
+       // updateQuotes()
+       cleanupFunction.current = false;
+       let interval = null;
+       interval = setInterval(() => {
+        //setSeconds(seconds => seconds + 1);
         updateQuotes()
-        return () =>  cleanupFunction.current = true;
+      }, 2000);
+        return () =>  {
+          cleanupFunction.current = true;
+          clearInterval(interval);
+        }
       }, [])
     )
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>{collectionQuotes.length}</Text>
+        {/* <Text>{collectionQuotes.length}</Text> */}
+        <Text>{seconds}</Text>
       </View>
     );
   }
